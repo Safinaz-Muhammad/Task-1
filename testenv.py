701858b5787442048b92ac8b25f700a1293047a8
Safinaz Muhammad <safinaz.mohamed07@eng-stu.cu.edu.eg>
2025-10-01 22:52:46 +0300

    fresh start

--- testenv.py
+++
@@ -1,6 +0,0 @@
-import torch
-import monai
-import SimpleITK as sitk
-print("Torch version: ", torch.__version__)
-print("MONAI version: ", monai.__version__)
-print("SimpleITK version: ", sitk.Version())
